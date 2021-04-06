37a9fa66bcf1a38517dbe9c5356581a619e0ba40
instantRahmen <erika@rahmen.io>
2021-04-06 07:37:20 -0400

    Add tsukimoon.net to project list

--- content/projects.js
+++ content/projects.js
@@ -1,4 +1,19 @@
 export default [
+  // Tsukimoon Website
+  {
+    name: 'Tsukimoon.net',
+    photo: '/images/projects/tsukimoon.net.png',
+    description: `
+A website created for my best friend and VTuber Tsukimoon. 
+It was built in Gatsby and uses a Sanity backend so she can update the content herself easily without having to rely on me to make changes for her.
+      `,
+    links: {
+      live: 'https://tsukimoon.net',
+      source: 'https://github.com/instantrahmen/tsukimoon-website',
+    },
+    tags: ['React', 'Sanity', 'Gatsby'],
+  },
+
   // Convention Center App
   {
     name: 'Chattanooga Convention Center Virtual Walkthrough',
@@ -19,7 +34,7 @@ An interface created for the Chattanooga Convention Center that allows potential
     photo: '/images/projects/moonbot.png',
 
     description: `
-A discord bot created for a close friend of mine. Allows her to quickly manage commands in a Sanity backend. Uses discord.js to make interfacing with the Discord API easier.
+A discord bot also created for Tsuki. Allows her to quickly manage commands in a Sanity backend. Uses discord.js to make interfacing with the Discord API easier.
   `,
     links: {
       live: null,
@@ -32,12 +47,21 @@ A discord bot created for a close friend of mine. Allows her to quickly manage c
   {
     name: 'Christmas Scavenger Hunt for VRMedia Pros',
     photo: '/images/projects/xmas-project.png',
-    description: `
-A virtual scavenger hunt created in Next.js using the Matterport API. The team hid items throughout a house and took 360 scans of said house.
-It was then my job to design and create this app as the sole developer at the company. The whole project went from concept to production in the span of 2-3 weeks.
+    description: /*md*/ `
+<p>
+A virtual scavenger hunt created in Next.js using the Matterport API.  
+</p>
+<br />
 
+<p>
+The team hid items throughout a house and took 360 scans of said house.
 The goal is to find all objects hidden around the house. 
 It won't accept an answer unless the item is in view.
+</p>
+<br />
+<p>
+The whole project went from concept to production in the span of 2-3 weeks.
+</p>
   `,
     links: {
       live: 'https://christmashunt.vrmediapros.com/',
